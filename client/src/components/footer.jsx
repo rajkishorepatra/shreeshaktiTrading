e23d493fc50d42c5580500273d00f664dede8649
Anurag Biswal <anugbswl@gmail.com>
2023-05-09 00:40:26 +0530

    footer

--- client/src/components/footer.jsx
+++ client/src/components/footer.jsx
@@ -1,14 +1,15 @@
 import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import Link from '@mui/material/Link';
 
 function Copyright() {
   return (
-    <Typography variant="body2" color="textSecondary">
+    <Typography variant="body2" color="text.secondary">
       {'Copyright © '}
       <Link color="inherit" href="https://mui.com/">
-        Ruchi Website
+        Your Website
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
@@ -16,15 +17,17 @@ function Copyright() {
   );
 }
 
-
-
 export default function PageFooter() {
-
-
   return (
-    <div >
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        minHeight: '100vh',
+      }}
+    >
       <CssBaseline />
-      <Container component="main" maxWidth="sm">
+      <Container component="main" sx={{ mt: 8, mb: 2 }} maxWidth="sm">
         <Typography variant="h2" component="h1" gutterBottom>
           Sticky footer
         </Typography>
@@ -34,12 +37,25 @@ export default function PageFooter() {
         </Typography>
         <Typography variant="body1">Sticky footer placeholder.</Typography>
       </Container>
-      <footer >
+      <Box
+        component="footer"
+        sx={{
+          py: 3,
+          px: 2,
+          mt: 'auto',
+          backgroundColor: (theme) =>
+            theme.palette.mode === 'light'
+              ? theme.palette.grey[200]
+              : theme.palette.grey[800],
+        }}
+      >
         <Container maxWidth="sm">
-          <Typography variant="body1">My sticky footer can be found here.</Typography>
+          <Typography variant="body1">
+            My sticky footer can be found here.
+          </Typography>
           <Copyright />
         </Container>
-      </footer>
-    </div>
+      </Box>
+    </Box>
   );
 }
