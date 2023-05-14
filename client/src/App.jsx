bf149973c9bfde715478ba6f007486f61b7a529b
Anurag Biswal <anugbswl@gmail.com>
2023-05-14 22:14:39 +0530

    changes for merge

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,8 +3,6 @@ import Navbar from "./components/navbar";
 import Quote from "./components/quote";
 import Why from "./components/why";
 import Services from "./components/services";
-import { Box } from "@mui/material";
-
 
 // import components
 import PageFooter from "./components/footer";
@@ -16,24 +14,14 @@ export default function App() {
   return (
     <>
       <Navbar />
-      <Box className="main content">
-        {/* <Box sx={boxStyle}>
-        <h1>OUR SERVICES</h1>
-      <Stack direction="row" spacing={2} sx={stackView}>
-        <Item>Item 1</Item>
-        <Item>Item 2</Item>
-        <Item>Item 3</Item>
-      </Stack>
-      </Box> */}
-      </Box>
-
+      <PageHeader />
       {/* <Services /> */}
 
       <Why />
       <Quote />
-      <PageHeader />
-      <NumbersSection />
+
       <Clients />
+      <NumbersSection />
       <PageFooter />
     </>
   );
