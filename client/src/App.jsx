0d0fe033ad0da610a28b7f88db632927af842f5a
Anurag Biswal <anugbswl@gmail.com>
2023-05-15 10:58:33 +0530

    navbar changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,18 @@
-import "./App.css";
-import Navbar from "./components/navbar";
-import Quote from "./components/quote";
-import Why from "./components/why";
-import Services from "./components/services";
-
 // import components
 import PageFooter from "./components/footer";
 import Clients from "./components/clients";
 import PageHeader from "./components/header";
 import NumbersSection from "./components/numbers";
+import Navbar from "./components/navbar";
+import Quote from "./components/quote";
+import Why from "./components/why";
 
 export default function App() {
   return (
     <>
       <Navbar />
       <PageHeader />
-      {/* <Services /> */}
-
+      
       <Why />
       <Quote />
 
--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -1,29 +1,84 @@
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Button from '@mui/material/Button';
-// import {Routes, Route, useNavigate} from 'react-router-dom';
-import "../App.css";
+import { css } from "@emotion/react";
+
+import { Menu as MenuIcon } from "@mui/icons-material";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import { Link } from "@mui/material";
 
 export default function Navbar() {
-    return (
+  const styles = {
+    logo: css`
+      width: 100px;
+    `,
+    menuButton: css`
+      margin-right: 1rem;
+      color: #000000;
+      @media (min-width: 768px) {
+        display: none;
+      }
+    `,
+    links: css`
+      display: none;
+      @media (min-width: 768px) {
+        display: flex;
+      }
+    `,
 
-      <>
-      <Box sx={{ flexGrow: 1, justifyContent: "space-between", display:"flex"}}>
-        <AppBar position="static" sx={{width: '100vw', background:'white', boxShadow:0}}>
-          <Toolbar>
-            <Button color="inherit" sx={{fontFamily: 'Arizonia, cursive', fontSize: '2rem', fontWeight:"bolderx", }}><span style={{fontSize: '5rem', color:"#4d4dff", fontWeight:"bolder"}}>R</span>UCHI</Button>
-            <div className="details">
-            <Button color="inherit">
-              Home
-            </Button>
-            <Button color="inherit">Tracking</Button>
-            <Button color="inherit">Services</Button>
-            <Button color="inherit">Admin</Button>
-            </div>
-          </Toolbar>
-        </AppBar>
-      </Box>
-      </>
-    )
+    link: css`
+      color: #000000;
+      text-decoration: none;
+      margin-right: 1rem;
+      &:hover {
+        text-decoration: underline;
+      }
+    `,
+  };
+
+  return (
+    <Box sx={{ background: "#1a2f43" }}>
+      <AppBar>
+        <Toolbar>
+          <Container maxWidth="lg">
+            <Box
+              sx={css`
+                display: flex;
+                justify-content: space-between;
+              `}
+            >
+              <Typography sx={styles.logo} variant="h6" noWrap>
+                Logo
+              </Typography>
+              <IconButton
+                sx={styles.menuButton}
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+              >
+                <MenuIcon />
+              </IconButton>
+
+              <Box sx={styles.links}>
+                <Link sx={styles.link} href="#home">
+                  Home
+                </Link>
+                <Link sx={styles.link} href="#about">
+                  About
+                </Link>
+                <Link sx={styles.link} href="#services">
+                  Services
+                </Link>
+                <Link sx={styles.link} href="#contact">
+                  Contact
+                </Link>
+              </Box>
+            </Box>
+          </Container>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 }
