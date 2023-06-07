bb4fcfba6ca989d13253548351be8153f211df51
Anurag Biswal <anugbswl@gmail.com>
2023-06-08 00:29:37 +0530

    quote changes

--- client/src/components/quote.jsx
+++ client/src/components/quote.jsx
@@ -5,7 +5,7 @@ import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
-import {useState} from 'react';
+import { useState } from "react";
 
 import { css } from "@emotion/react";
 
@@ -16,38 +16,66 @@ import { motion } from "framer-motion";
 import quoteBackground from "../assets/quote-parallax.jpg";
 
 export default function Quote() {
-
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [mobile, setMobile] = useState('');
-  const [destinationTo, setDestinationTo] = useState('');
-  const [destinationFrom, setDestinationFrom] = useState('');
-  const [date, setDate] = useState('');
-  const [type, setType] = useState('');
-  const [message, setMessage] = useState('');
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [mobile, setMobile] = useState("");
+  const [destinationTo, setDestinationTo] = useState("");
+  const [destinationFrom, setDestinationFrom] = useState("");
+  const [date, setDate] = useState(new Date().format("DD-MM-YYYY"));
+  const [type, setType] = useState("");
+  const [message, setMessage] = useState("");
 
   const quoteForm = async (e) => {
     e.preventDefault();
-    console.log({name}, {email}, {mobile}, {destinationTo}, {destinationFrom}, {date}, {type}, {message});
+    console.log(
+      { name },
+      { email },
+      { mobile },
+      { destinationTo },
+      { destinationFrom },
+      { date },
+      { type },
+      { message }
+    );
     const response = await fetch("http://localhost:3001/quote", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({name, email, mobile, destinationTo, destinationFrom, date, type, message}),
-    }).then((res) => res.json())
-    .then(async (res) => {
-      const resData = await res;
-      console.log(resData);
-      if(resData.status === "success") {
-        alert("Message sent");
-      } else if(resData.status === "fail") {
-        alert("Message failed to send");
-      }
+      body: JSON.stringify({
+        name,
+        email,
+        mobile,
+        destinationTo,
+        destinationFrom,
+        date,
+        type,
+        message,
+      }),
     })
-    .then(() => {{name, email, mobile, destinationTo, destinationFrom, date, type, message}});
+      .then((res) => res.json())
+      .then(async (res) => {
+        const resData = await res;
+        console.log(resData);
+        if (resData.status === "success") {
+          alert("Message sent");
+        } else if (resData.status === "fail") {
+          alert("Message failed to send");
+        }
+      })
+      .then(() => {
+        {
+          name,
+            email,
+            mobile,
+            destinationTo,
+            destinationFrom,
+            date,
+            type,
+            message;
+        }
+      });
   };
-
 
   const styles = {
     heading: css`
@@ -88,10 +116,81 @@ export default function Quote() {
       }
     `,
     formInputField: css`
-      background-color: white;
-      border-radius: 0.25rem 0.25rem 0 0;
+      input[type="number"]::-webkit-inner-spin-button,
+      input[type="number"]::-webkit-outer-spin-button {
+        -webkit-appearance: none;
+        margin: 0;
+      }
+
+      & label {
+        color: #999;
+      }
+      & label.Mui-focused {
+        color: white;
+      }
+      & input {
+        color: white;
+      }
+
+      & textarea {
+        color: white;
+      }
+
+      & label.Mui-focused {
+        color: white;
+      }
+      & .MuiInput-underline:after {
+        border-bottom-color: white;
+      }
+      & .MuiOutlinedInput-root {
+        & fieldset {
+          border-color: #999;
+        }
+        &:hover fieldset {
+          border-color: white;
+        }
+        &.Mui-focused fieldset {
+          border-color: #ddd;
+        }
+      }
+    `,
+    datePickerField: css`
+      width: 100%;
+      svg {
+        color: #999;
+      }
+      svg: hover {
+        color: white;
+      }
+      & label {
+        color: #999;
+      }
+      & label.Mui-focused {
+        color: #999;
+      }
+      & input {
+        color: white;
+      }
+      & .MuiOutlinedInput-root {
+        & fieldset {
+          border-color: #999;
+        }
+        &:hover fieldset {
+          border-color: white;
+        }
+        &.Mui-focused fieldset {
+          border-color: #ddd;
+        }
+      }
     `,
   };
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    console.log(date);
+    console.log(name);
+  };
+
   return (
     <>
       <Box sx={styles.quoteContainer}>
@@ -110,16 +209,13 @@ export default function Quote() {
                 </Typography>
               </Box>
               <Box>
-                <form>
+                <form onSubmit={(e) => handleClick(e)}>
                   <Grid container spacing={1}>
                     <Grid item xs={12}>
                       <TextField
                         fullWidth
-                        hiddenLabel
                         id="name"
-                        size="small"
-                        variant="filled"
-                        placeholder="Full Name"
+                        label="Full Name"
                         sx={styles.formInputField}
                         value={name}
                         onChange={(e) => setName(e.target.value)}
@@ -128,12 +224,9 @@ export default function Quote() {
 
                     <Grid item xs={12} sm={6}>
                       <TextField
-                        hiddenLabel
                         fullWidth
                         id="email"
-                        size="small"
-                        variant="filled"
-                        placeholder="Email"
+                        label="Email"
                         sx={styles.formInputField}
                         value={email}
                         onChange={(e) => setEmail(e.target.value)}
@@ -142,28 +235,23 @@ export default function Quote() {
 
                     <Grid item xs={12} sm={6}>
                       <TextField
-                        hiddenLabel
                         //
                         fullWidth
                         id="mobile"
-                        size="small"
-                        variant="filled"
-                        placeholder="Mobile"
+                        label="Mobile"
                         sx={styles.formInputField}
                         value={mobile}
                         onChange={(e) => setMobile(e.target.value)}
+                        type="number"
                       />
                     </Grid>
 
                     <Grid item xs={12} sm={6}>
                       <TextField
-                        hiddenLabel
                         //
                         fullWidth
                         id="destinationTo"
-                        size="small"
-                        variant="filled"
-                        placeholder="Destination To"
+                        label="Destination To"
                         sx={styles.formInputField}
                         value={destinationTo}
                         onChange={(e) => setDestinationTo(e.target.value)}
@@ -172,12 +260,9 @@ export default function Quote() {
 
                     <Grid item xs={12} sm={6}>
                       <TextField
-                        hiddenLabel
                         fullWidth
                         id="destinationFrom"
-                        size="small"
-                        variant="filled"
-                        placeholder="Destination From"
+                        label="Destination From"
                         sx={styles.formInputField}
                         value={destinationFrom}
                         onChange={(e) => setDestinationFrom(e.target.value)}
@@ -186,12 +271,9 @@ export default function Quote() {
 
                     <Grid item xs={12} sm={6}>
                       <TextField
-                        hiddenLabel
                         fullWidth
                         id="shipmentType"
-                        size="small"
-                        variant="filled"
-                        placeholder="Shipping Type"
+                        label="Shipping Type"
                         sx={styles.formInputField}
                         value={type}
                         onChange={(e) => setType(e.target.value)}
@@ -199,34 +281,34 @@ export default function Quote() {
                     </Grid>
 
                     <Grid item xs={12} sm={6}>
-                      <TextField
-                        hiddenLabel
-                        fullWidth
-                        id="date"
-                        size="small"
-                        variant="filled"
-                        placeholder="Date"
-                        sx={styles.formInputField}
+                      <DatePicker
+                        label="Date"
+                        sx={styles.datePickerField}
                         value={date}
-                        onChange={(e) => setDate(e.target.value)}
+                        onChange={(e) => setDate(e)}
+                        renderInput={(params) => <TextField {...params} />}
                       />
                     </Grid>
 
                     <Grid item xs={12}>
                       <TextField
-                        hiddenLabel
                         id="Messege"
                         multiline
                         rows={4}
                         fullWidth
-                        placeholder="Messege"
+                        label="Messege"
                         sx={styles.formInputField}
                         value={message}
                         onChange={(e) => setMessage(e.target.value)}
                       />
                     </Grid>
                     <Grid item xs={12}>
-                      <Button variant="contained" size="large" onClick={quoteForm}>
+                      <Button
+                        variant="contained"
+                        size="large"
+                        // onClick={quoteForm}
+                        type="submit"
+                      >
                         Submit
                       </Button>
                     </Grid>
